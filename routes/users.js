4484978760ba9bebfa9e8cb4fa77c6a8306ebcc5
sebastianalvirap <juanoalvira97@gmail.com>
2024-10-09 20:10:14 -0500

    create, publications, models and route-users

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { testUser } from "../controllers/user.js";
 
 const router = Router();
 
+// Definir rutas de user
+router.get('/test-user', testUser);
 
-//Exportar el router
+//Exportar el Router
 export default router;
